e87c77245df31bf2adaad96d4a9661c54765f425
Hanna Liz <hannaliz@MacBook-Pro-de-Hanna.local>
2024-09-16 19:46:33 -0300

    feat: onesignal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { Loading } from "./src/components/Loading";
+import { NativeBaseProvider } from "native-base";
+import { OneSignal } from "react-native-onesignal";
+import { Routes } from "./src/routes";
+import { StatusBar } from "react-native";
+import { THEME } from "./src/theme";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
-
-import { CartContextProvider } from './src/contexts/CartContext';
+OneSignal.initialize("d00c84df-8c28-4f06-99aa-f2216b78cb80");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
